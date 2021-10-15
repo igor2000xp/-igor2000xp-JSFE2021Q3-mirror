25f142eca557d68609d85d237fdad03ddf627b43
igor2000xp <igorring@gmail.com>
2021-10-15 20:38:35 +0300

    fix: fixing some problem with Video player 200

--- museum-dom/src/dop-files-js/js/script.js
+++ museum-dom/src/dop-files-js/js/script.js
@@ -1,39 +1,3 @@
-// new Swipper('.image-slider', {
-//    // arrows
-//    navigation: {
-//       nextEl: 'swiper-button-next',
-//       prevEl: 'swiper-button-prev'
-//    },
-//    // bullets
-//    pagination: {
-//       el: 'swiper-pagination',
-//       clicable: 'true',
-//    }
-
-// });
-
-// const swiper = new Swiper('.swiper', {
-//    // Optional parameters
-//    direction: 'vertical',
-//    loop: true,
- 
-//    // If we need pagination
-//    pagination: {
-//      el: '.swiper-pagination',
-//    },
- 
-//    // Navigation arrows
-//    navigation: {
-//      nextEl: '.swiper-button-next',
-//      prevEl: '.swiper-button-prev',
-//    },
- 
-//    // And if we need scrollbar
-//    scrollbar: {
-//      el: '.swiper-scrollbar',
-//    },
-//  });
-
 const swiperImage = new Swiper('.swiper-image', {
    // Optional parameters
    // direction: 'vertical',
