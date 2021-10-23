1c19822cd32ec2379325650cbb33f75ddbbaf874
igor2000xp <igorring@gmail.com>
2021-10-24 00:50:20 +0300

    fix: fix timezone  305

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,9 +2,11 @@ const time = document.querySelector('.time');
 const dateDoc = document.querySelector('.date');
 // const options = { weekday: 'long', month: 'long', day: 'numeric' };
 const options = {month: 'long', day: 'numeric', hour: '2-digit', minute: 'numeric', timeZone: 'UTC'};
-const optionsTime = {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'Europe/Moscow'};
+const optionsTime = {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'Europe/Moscow'};
 options.timeZone = 'UTC';
 
+// hour12: true, 
+// hour12: false,
 function showDate() {
   const date = new Date();
   const currentDate = date.toLocaleDateString('ru-RU', options);
