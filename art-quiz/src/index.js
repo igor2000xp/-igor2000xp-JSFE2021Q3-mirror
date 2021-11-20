1de38fbdf9751b48e88f5031e3fb5b1bedda2813
igor2000xp <igorring@gmail.com>
2021-11-20 11:03:32 +0300

    fix: .DS_Store Sat Nov 20 11:03:32 +03 2021

--- art-quiz/src/index.js
+++
@@ -1,7 +0,0 @@
-import { _ } from 'framework';
-import { bootstrap } from './framework/core/functions/bootstrap';
-import { appModule } from "./app/app.module";
-
-_.delay(1000).then(() => {
-  bootstrap(appModule);
-})
