fdae282fb9d5136e8eb09c78e07aff0d2e80a6e6
igor2000xp <igorring@gmail.com>
2021-11-21 07:21:00 +0300

    fix: fix some folders Sun Nov 21 07:20:58 +03 2021

--- art-quiz/old/src/framework/index.js
+++
@@ -1,8 +0,0 @@
-import { Module as WFMModule } from './core/module';
-import { Component as WFMComponent } from './core/component';
-import { bootstrap } from './core/bootstrap';
-
-export { 
-  WFMModule,
-  WFMComponent,
-}
