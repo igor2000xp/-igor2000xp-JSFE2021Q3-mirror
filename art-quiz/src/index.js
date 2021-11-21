1f33d42f10aebd3b12d75af5cdc529c71e29ad94
igor2000xp <igorring@gmail.com>
2021-11-21 12:23:18 +0300

    feat; tune css Sun Nov 21 12:23:18 +03 2021

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -1,6 +1,9 @@
 import { _ } from 'framework';
 import { bootstrap } from './framework/core/functions/bootstrap';
 import { appModule } from "./app/app.module";
+import './css/materialize.min.css';
+import './css/normalize.css';
+import './css/style.css';
 
 _.delay(1000).then(() => {
   bootstrap(appModule);
--- art-quiz/webpack.config.js
+++ art-quiz/webpack.config.js
@@ -1,13 +1,14 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// let mode = 'development'
-// if (process.env.NODE_ENV === 'production') {
-//     mode = 'production'
-// }
-// console.log(mode + ' mode')
 
+let mode = 'development';
+if(process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+console.log(mode + ' node');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -30,11 +31,51 @@ module.exports = {
       'framework': path.join(__dirname, 'src/framework')
     }
   },
+
    
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
-      title: 'Native Framework',
+      title: 'Art-quiz',
       template: './index.html',
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+            (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+                loader: "postcss-loader",
+                options: {
+                    postcssOptions: {
+                        plugins: [
+                            [
+                                "postcss-preset-env",
+                                {
+                                    // Options
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+            "sass-loader",
+        ],
+    }
+
+    ]
+  },
+
+  mode: mode
+
 }
