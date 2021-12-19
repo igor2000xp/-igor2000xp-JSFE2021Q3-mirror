7537938526f5e5a1a28ffd298aba0f8427ca8259
igor2000xp <igorring@gmail.com>
2021-12-19 08:07:36 +0300

    feat: plug event-bus

---
+++ christmas-task-1/src/framework.ts/utils/event-bus.ts
@@ -0,0 +1,41 @@
+const DEFAULT_EVENT = 'default';
+export const ALL_EVENTS = 'ALL_EVENTS';
+
+interface Subscription {
+  event: string,
+  handler: Handler,
+}
+
+export type Handler = () => void;
+
+export default class EventBus {
+  private subscriptions: Subscription[] = [];
+
+  constructor() {}
+
+  subscribe(eventType: string = DEFAULT_EVENT, handler: Handler): void {
+    if (this.subscriptions.find((sub) => eventType === sub.event && sub.handler === handler)) return;
+
+    this.subscriptions.push({
+      event: eventType,
+      handler,
+    });
+  }
+
+  unSubscribe(eventType: string = DEFAULT_EVENT, handler: Handler): void {
+    const subItem = this.subscriptions.find((sub) => eventType === sub.event && sub.handler === handler);
+    let subIndex: number = -1;
+    if(subItem !== undefined) {
+      subIndex = this.subscriptions.indexOf(subItem);
+    }
+    if (subIndex !== -1) {
+      this.subscriptions.splice(subIndex, 1);
+    }
+  }
+
+  emmit(eventType: string) {
+    this.subscriptions
+      .filter((sub) => sub.event === eventType || sub.event === ALL_EVENTS)
+      .forEach(({ handler }) => handler());
+  }
+}
